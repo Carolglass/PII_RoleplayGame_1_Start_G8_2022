aa7be02a6c6040df8b9b8b8e94181e43e79cfec0
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2022-04-27 11:39:39 -0300

    Clase Mago

---
+++ src/Library/Mago.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    using System;
+
+    public class Mago
+    {
+        private List<Items> Itemlist = new List<Items> ();
+        public Mago(string nickname, int lives, int damage, string power)
+        {
+            this.Nickname = nickname;
+            this.Life = lives;
+            this.Damage = damage;
+            this.Power = power;
+        }
+
+        public string Nickname { get; }
+
+        public int Life { get; private set; }
+
+        public int Damage { get; }
+
+        public string Power { get; }
+
+        public void Attack(int liveEnemy)
+        {
+            enemy.Life = enemy.Life - this.Damage;
+        }
+
+        public bool Healthy()
+        {
+            return this.Life > 0;
+        }
+    }
+}
