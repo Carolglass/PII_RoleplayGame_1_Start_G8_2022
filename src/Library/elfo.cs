ff16f3b24c53587bf7ab666b75896f7db9a04345
Clarensedor <101676526+Clarensedor@users.noreply.github.com>
2022-04-27 22:18:55 -0300

    Se hacen cambio en las clases
    blabla

---
+++ src/Library/elfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elfo
+{
+    using System;
+
+    public class Character
+    {
+        public Character(string nickname, int lives, int damage, string power)
+        {
+            this.Nickname = nickname;
+            this.Life = lives;
+            this.Damage = damage;
+            this.Power = power;
+        }
+
+        public string Nickname { get; }
+
+        public int Life { get; private set; }
+
+        public int Damage { get; }
+
+        public string Power { get; }
+
+        public void Attack(Character enemy)
+        {
+            enemy.Life = enemy.Life - this.Damage;
+        }
+
+        public bool Healthy()
+        {
+            return this.Life > 0;
+        }
+    }
+}
