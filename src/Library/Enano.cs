d0c173bad5e3477514d65304b86677fffbf9f925
juang0nz <101676463+juang0nz@users.noreply.github.com>
2022-04-27 23:07:51 -0300

    Creada la Clase Enano

---
+++ src/Library/Enano.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    using System;
+
+    public class Enano
+    {
+       
+        private string nombre;
+        private int vida;
+        private int danio;
+
+
+        public Enano (string nombre, int vida, int danio)
+        {
+            this.Nombre = nombre;
+            this.Vida = vida;
+            this.Danio = danio;
+            
+        }
+
+        public string Nombre { get; }
+
+        public int Vida { get; private set; }
+
+        public int Danio { get; }
+
+
+
+        public void RecibirDanio(int daniorecibido)
+        {
+            this.vida-=daniorecibido;
+        }
+
+        public bool Vivo()
+        {
+            return this.vida > 0;
+        }
+    }
+}
