6e62c4cd890ca353245cc6e54a689015322c783d
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2022-04-30 18:53:46 -0300

    Update Pico.cs

--- src/Library/Items/Pico.cs
+++ src/Library/Items/Pico.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Library.Items
 {
-    class Pico
+    public class Pico
     {
+        private string nombre;
+        private int valorAtaque;
+        private static List<int> charactersId = new List<int>();
+
+        public String Nombre { get; private set; }
+        public int ValorAtaque { get; private set; }
+
+        public Pico(string nombre, int valorAtaque)
+        {
+            Nombre = this.nombre;
+            ValorAtaque = this.valorAtaque;
+        }
+        public static void CharactersId(int identificador)
+        {
+            charactersId.Add(identificador);
+        }
+
     }
+}
 }
