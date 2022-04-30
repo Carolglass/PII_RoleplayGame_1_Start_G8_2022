e97bec749f8d75752316d352674806a32a9439ed
juang0nz <101676463+juang0nz@users.noreply.github.com>
2022-04-30 17:25:09 -0300

    Baston y Casco

--- src/Library/Items/Casco.cs
+++ src/Library/Items/Casco.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace Library.Items
 {
     internal class Casco
-    {
+    { 
+        private string nombre;
+        private int valorDefensa;
+        private static List<int> charactersId = new List<int>();
+
+        public String Nombre{ get; private set; }
+        public int valorDefensa { get; private set; } 
+
+        public Casco(string nombre, int valorDefensa)
+        {
+            Nombre = this.nombre;
+            valorDefensa = this.valorDefensa;
+        }
+            public static void CharactersId(int identificador)
+        {
+            charactersId.Add(identificador);
+        }
     }
 }
--- src/Library/Mago.cs
+++ src/Library/Mago.cs
@@ -27,6 +27,21 @@ namespace Library
         public bool Healthy()
         {
             return this.Vida > 0;
+
         }
+        public void AumentarVida(int VidaAumentada)
+        {
+            this.vida += VidaAumentada;
+        }
+      //  if (Id==1)
+       //     {
+        //      Mago.AumentarVida(Vida)
+         //   }
+	}
+
+	}
+	{
+
+	}
     }
 }
